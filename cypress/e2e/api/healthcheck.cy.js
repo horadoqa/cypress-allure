b93e6bd6f8af51c65eaabb63d05fd42665c91e08
Ricardo Fahham <rfahham@hotmail.com>
2025-12-08 17:30:31 -0300

    adicionando feature estory no healthcheck

--- cypress/e2e/api/healthcheck.cy.js
+++ cypress/e2e/api/healthcheck.cy.js
@@ -1,6 +1,10 @@
 const allure = Cypress.Allure.reporter.getInterface();
 
 describe('Cenário 1 - HealthCheck', () => {
+
+    allure.feature('API');
+    allure.story('Healthcheck');
+    
     it('should return 200 OK when accessing serverest.dev', () => {
       cy.request({
         method: 'GET',
