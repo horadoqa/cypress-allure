05d13527dc2c4d20f1714098f90e8c90fc530d7c
Ricardo Fahham <rfahham@hotmail.com>
2025-12-09 09:17:04 -0300

    Corrigindo cenário Login Inválido

--- cypress/e2e/usuario/login-invalido.cy.js
+++ cypress/e2e/usuario/login-invalido.cy.js
@@ -5,43 +5,24 @@ describe('Login Inválido', () => {
   before(() => {
     allure.feature('Usuário');
     allure.story('Login');
-
-    // garantir que variáveis estão definidas
-    expect(Cypress.env('emailInvalido')).to.be.a('string');
-    expect(Cypress.env('passwordInvalido')).to.be.a('string');
   });
   
   it('Não deve realizar login', () => {
-
-    allure.startStep('Enviar requisição com credenciais inválidas');
-
-    const bodyRequest = {
-      email: Cypress.env('emailInvalido'),
-      password: Cypress.env('userPassword')
-    };
-
-    allure.attachment('Request Body', JSON.stringify(bodyRequest), 'application/json');
-
     cy.request({
       method: 'POST',
       url: 'https://serverest.dev/login',
-      failOnStatusCode: false,
+      failOnStatusCode: false,   // importante para permitir 401
       headers: {
         accept: 'application/json',
         'Content-Type': 'application/json'
       },
-      body: bodyRequest
-    }).should((response) => {
-
-      allure.startStep('Validar resposta');
-      allure.attachment('Response Body', JSON.stringify(response.body), 'application/json');
-
+      body: {
+        email: Cypress.env('emailInvalido'),
+        password: Cypress.env('userPassword')
+      }
+    }).then((response) => {
       expect(response.status).to.eq(401);
       expect(response.body).to.have.property('message', 'Email e/ou senha inválidos');
-
-      allure.endStep();
     });
-
-    allure.endStep();
   });
 });
